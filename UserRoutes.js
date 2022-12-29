256af1522a3c69b1900a8cf6ff649b1c71712caf
vedant <ursvedantyetekar@gmail.com>
2022-12-30 02:05:33 +0530

    cleared unused files

--- UserRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getForm, registerUser } = require("./UserController");
-
-const router = express.Router();
-
-router.route("/").get(getForm);
-router.route("/register").post(registerUser);
-
-module.exports = router;
